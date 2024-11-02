ff5d582665a7ffab99111a930ce9270769042e32
Kaito <134911191+kaito1105@users.noreply.github.com>
2024-11-02 00:24:05 -0500

    Add files via upload

---
+++ lecture/LEC-01/A/hello.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // print("HELLO", "My Name Is")
+    // System.out.println("HELLO");
+    // System.out.println("My Name Is");
+
+    cout << "HELLO\n";
+    cout << "My Name Is" << endl;
+    cout << "Kaito Miyamoto\n"
+        << "You Killed My Father" << endl
+        << "Prepare to Die" << endl;
+
+    return 0;
+}
